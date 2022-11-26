80427b02c12f4003902a4d66361f42200ec46b60
Jaz Kime <JazDKime@gmail.com>
2022-11-26 12:54:39 -0800

    Created ContentContainer and established content height

--- client-side/src/App.tsx
+++ client-side/src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import ContentContainer from './components/ContentContainer';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      
+      <ContentContainer />
     </div>
   );
 }
---
+++ client-side/src/components/ContentContainer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function ContentContainer() {
+	return <section id="content-container">
+        Hello!
+    </section>;
+}
+
+export default ContentContainer;
