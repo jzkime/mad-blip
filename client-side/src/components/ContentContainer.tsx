cb2702a04e370abb025048cb0414c83fab7e2925
Jaz Kime <JazDKime@gmail.com>
2022-11-26 14:05:23 -0800

    Story Added to ContentContainer

--- client-side/src/components/ContentContainer.tsx
+++ client-side/src/components/ContentContainer.tsx
@@ -1,8 +1,16 @@
 import React from "react";
+import Story from './Story'
+
+/**
+ * Todo:
+ *  Button: generate call to backend that will fetch random words
+ *  Story Component: pass in words
+ *  Button: Clear inputs
+ */
 
 function ContentContainer() {
 	return <section id="content-container">
-        Hello!
+        <Story />
     </section>;
 }
 
