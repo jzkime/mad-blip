cf5de6f84f29304157d3bfbfda5c6cc6d2cdde83
Jaz Kime <JazDKime@gmail.com>
2022-11-29 11:48:56 -0800

    Cleanup and formatting

--- data/seeds/00-wordtypes.js
+++ data/seeds/00-wordtypes.js
@@ -1,12 +1,14 @@
-exports.seed = async function(knex) {
-  await knex('word_types').truncate().insert([
-    {wordtype_id: 1, word_type: 'adjective'},
-    {wordtype_id: 2, word_type: 'adverb'},
-    {wordtype_id: 3, word_type: 'nounThing'},
-    {wordtype_id: 4, word_type: 'nounThingPlural'},
-    {wordtype_id: 5, word_type: 'nounPlace'},
-    {wordtype_id: 6, word_type: 'nounAnimal'},
-    {wordtype_id: 7, word_type: 'verbPast'},
-    {wordtype_id: 8, word_type: 'verbPresent'},
-  ]);
+exports.seed = async function (knex) {
+	await knex("word_types")
+		.truncate()
+		.insert([
+			{ wordtype_id: 1, word_type: "adjective" },
+			{ wordtype_id: 2, word_type: "adverb" },
+			{ wordtype_id: 3, word_type: "nounThing" },
+			{ wordtype_id: 4, word_type: "nounThingPlural" },
+			{ wordtype_id: 5, word_type: "nounPlace" },
+			{ wordtype_id: 6, word_type: "nounAnimal" },
+			{ wordtype_id: 7, word_type: "verbPast" },
+			{ wordtype_id: 8, word_type: "verbPresent" },
+		]);
 };
--- src/words/words.controller.ts
+++ src/words/words.controller.ts
@@ -1,16 +1,14 @@
 import { Controller, Get, Param, Query, Req } from "@nestjs/common";
-import { Request } from "express";
 import { WordsService } from "./words.service";
-@Controller('words')
+@Controller("words")
 export class WordsController {
 	constructor(private readonly wordsService: WordsService) {}
 	@Get()
 	async getWords() {
-        console.log('yo')
 		return await this.wordsService.findAllWords();
 	}
-	@Get('/:type') 
-	async getWordTypeLimit(@Param('type') param, @Query('limit') query) {
+	@Get("/:type")
+	async getWordTypeLimit(@Param("type") param, @Query("limit") query) {
 		return await this.wordsService.findWordsOfType(param, query || 1);
 	}
 }
