f28bf2292f56993515b2189030724bbe60b2ccf3
Jaz Kime <JazDKime@gmail.com>
2022-11-29 15:36:15 -0800

    Server now allows requesting resources from different origins

--- server-side/src/main.ts
+++ server-side/src/main.ts
@@ -1,10 +1,16 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
 const PORT = process.env.PORT || 9000;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(PORT);
+	const app = await NestFactory.create(AppModule);
+	app.enableCors({
+		origin: true,
+		methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+		allowedHeaders: "XMLHttpRequest, Access-Control-Allow-Origin",
+		credentials: true,
+	});
+	await app.listen(PORT);
 }
 bootstrap();
