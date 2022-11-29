1bf5b2d3d3184ca03c606d112475388afe795afd
Jaz Kime <JazDKime@gmail.com>
2022-11-29 12:17:59 -0800

    GET /subjects/:type returns with random subject from db

--- src/subjects/subjects.service.ts
+++ src/subjects/subjects.service.ts
@@ -11,4 +11,13 @@ export class SubjectsService {
 			.leftJoin("subject_types as st", "s.sub_type", "=", "st.subtype_id")
 			.select("s.sub_id", "s.subject_name", "st.subject_type");
 	}
+
+	findSpecificSubjects(type, limit) {
+		return this.knex("subjects as s")
+			.leftJoin("subject_types as st", "s.sub_type", "=", "st.subtype_id")
+			.select("s.sub_id", "s.subject_name", "st.subject_type")
+			.where("st.subtype_id", type)
+			.limit(limit)
+			.orderByRaw("random()");
+	}
 }
