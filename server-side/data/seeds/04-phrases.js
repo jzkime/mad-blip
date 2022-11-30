616f2bbe7ce12fdf13924e638d00f16d36658c4e
Jaz Kime <JazDKime@gmail.com>
2022-11-30 09:33:45 -0800

    More phrases added to database seeds

--- server-side/data/seeds/04-phrases.js
+++ server-side/data/seeds/04-phrases.js
@@ -11,5 +11,16 @@ exports.seed = async function (knex) {
 			{ phrase: "May the Force be with you." },
 			{ phrase: "My mama always said life was like a box of chocolates." },
 			{ phrase: "Just keep swimming." },
+			{ phrase: "Hello there." },
+			{ phrase: "Fine day for a stroll." },
+			{ phrase: "Today is a new day." },
+			{ phrase: "This is a new beginning." },
+			{ phrase: "I'm going to be better from now on." },
+			{ phrase: "I'm perfect just the way I am." },
+			{ phrase: "I'm enough." },
+			{ phrase: "Nice weather we're having." },
+			{ phrase: "For sparta." },
+			{ phrase: "How am I supposed to live, laugh, love in these conditions?" },
+			{ phrase: "Today I'm the wisest I've ever been." },
 		]);
 };
