6f9ce6c0af8e7d7a7871c075bf13eb4c4f95f37c
Jaz Kime <JazDKime@gmail.com>
2022-11-29 20:17:37 -0800

    Inputs now have placeholders

--- client-side/src/components/StoryPrompts.tsx
+++ client-side/src/components/StoryPrompts.tsx
@@ -22,64 +22,64 @@ const StoryPrompts: React.FC<SetState> = ({ currentWords, setCurrentWords }) => 
 
 	return (
 		<form id="prompt-form">
-			<label>Creature 1:</label>
+			<label>Creature1</label>
 			<input
 				name="creature1"
 				value={currentWords.creature1}
 				onChange={handleChange}
 				placeholder="dragon"
 			/>
-			<label>Pet Name:</label>
+			<label>Name</label>
 			<input
 				name="petName"
 				value={currentWords.petName}
 				onChange={handleChange}
 				placeholder="Blurg"
 			/>
-			<label>Adj</label>
-			<input name="adj1" value={currentWords.adj1} onChange={handleChange} />
-			<label>Name:</label>
-			<input name="wizardName" value={currentWords.wizardName} onChange={handleChange} />
+			<label>Adjective </label>
+			<input name="adj1" value={currentWords.adj1} onChange={handleChange} placeholder="cheerful" />
+			<label>Name</label>
+			<input name="wizardName" value={currentWords.wizardName} onChange={handleChange} placeholder="Gandalf" />
 			<label>Specialty</label>
-			<input name="specialty" value={currentWords.specialty} onChange={handleChange} />
+			<input name="specialty" value={currentWords.specialty} onChange={handleChange} placeholder="dancing" />
 			<label>Place</label>
-			<input name="destination" value={currentWords.place1} onChange={handleChange} />
+			<input name="destination" value={currentWords.place1} onChange={handleChange} placeholder="cave" />
 			<label>Noun Plural</label>
-			<input name="noun1" value={currentWords.noun1} onChange={handleChange} />
+			<input name="noun1" value={currentWords.noun1} onChange={handleChange} placeholder="flowers" />
 			<label>Noun Plural</label>
-			<input name="noun2" value={currentWords.noun2} onChange={handleChange} />
+			<input name="noun2" value={currentWords.noun2} onChange={handleChange} placeholder="gems" />
 			<label>Noun Plural</label>
-			<input name="noun3" value={currentWords.noun3} onChange={handleChange} />
+			<input name="noun3" value={currentWords.noun3} onChange={handleChange} placeholder="berries" />
 			<label>Specific Time of Day</label>
-			<input name="timeOfDay" value={currentWords.timeOfDay} onChange={handleChange} />
+			<input name="timeOfDay" value={currentWords.timeOfDay} onChange={handleChange} placeholder="3am" />
 			<label>Place2</label>
-			<input name="home" value={currentWords.place2} onChange={handleChange} />
-			<label>Adj2</label>
-			<input name="adj2" value={currentWords.adj2} onChange={handleChange} />
+			<input name="home" value={currentWords.place2} onChange={handleChange} placeholder="cottage" />
+			<label>Adjective2</label>
+			<input name="adj2" value={currentWords.adj2} onChange={handleChange} placeholder="twisty" />
 			<label>Creature2</label>
-			<input name="creature2" value={currentWords.creature2} onChange={handleChange} />
-			<label>PastTense Verb</label>
-			<input name="pastV1" value={currentWords.pastV1} onChange={handleChange} />
-			<label>Phrase</label>
-			<input name="phrase1" value={currentWords.phrase1} onChange={handleChange} />
-			<label>PastTense Verb</label>
-			<input name="pastV2" value={currentWords.pastV2} onChange={handleChange} />
-			<label>Time of Year</label>
-			<input name="timeOfYear" value={currentWords.timeOfYear} onChange={handleChange} />
-			<label>Clothing piece</label>
-			<input name="outfit" value={currentWords.outfit} onChange={handleChange} />
-			<label>Verb</label>
-			<input name="presentV1" value={currentWords.presentV1} onChange={handleChange} />
-			<label>Adverb</label>
-			<input name="adv" value={currentWords.adv} onChange={handleChange} />
-			<label>Emotion</label>
-			<input name="emotion" value={currentWords.emotion} onChange={handleChange} />
-			<label>Phrase 2</label>
-			<input name="phrase2" value={currentWords.phrase2} onChange={handleChange} />
+			<input name="creature2" value={currentWords.creature2} onChange={handleChange} placeholder="goblin" />
 			<label>Past Tense Verb</label>
-			<input name="pastV3" value={currentWords.pastV3} onChange={handleChange} />
+			<input name="pastV1" value={currentWords.pastV1} onChange={handleChange} placeholder="smiled" />
+			<label>Phrase</label>
+			<input name="phrase1" value={currentWords.phrase1} onChange={handleChange} placeholder="Hello there." />
+			<label>Past Tense Verb</label>
+			<input name="pastV2" value={currentWords.pastV2} onChange={handleChange} placeholder="pulled" />
+			<label>Time of Year</label>
+			<input name="timeOfYear" value={currentWords.timeOfYear} onChange={handleChange} placeholder="last Summer" />
+			<label>Clothing piece</label>
+			<input name="outfit" value={currentWords.outfit} onChange={handleChange} placeholder="hat" />
+			<label>Verb</label>
+			<input name="presentV1" value={currentWords.presentV1} onChange={handleChange} placeholder="jump" />
+			<label>Adverb</label>
+			<input name="adv" value={currentWords.adv} onChange={handleChange} placeholder="gently" />
+			<label>Emotion</label>
+			<input name="emotion" value={currentWords.emotion} onChange={handleChange} placeholder="sad" />
+			<label>Phrase2</label>
+			<input name="phrase2" value={currentWords.phrase2} onChange={handleChange} placeholder="This is only the beginning." />
+			<label>Past Tense Verb</label>
+			<input name="pastV3" value={currentWords.pastV3} onChange={handleChange} placeholder="patted" />
 			<label>Present Tense Verb</label>
-			<input name="presentV2" value={currentWords.presentV2} onChange={handleChange} />
+			<input name="presentV2" value={currentWords.presentV2} onChange={handleChange} placeholder="laughing" />
 		</form>
 	);
 };
