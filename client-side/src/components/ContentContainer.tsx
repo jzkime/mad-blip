7ee623b401c629c5dfa3395a30a1eb1897ece59d
Jaz Kime <JazDKime@gmail.com>
2022-11-29 23:49:58 -0800

    handleClear function implemented to reset currentWords and remove corresponding items from localStorage

--- client-side/src/components/ContentContainer.tsx
+++ client-side/src/components/ContentContainer.tsx
@@ -1,11 +1,7 @@
 import React, { ReactElement, useState, useEffect } from "react";
 import Story from "./Story";
 import StoryPrompts from "./StoryPrompts";
-import {
-	AllWords,
-	initialWords,
-	OptionalWords,
-} from "../interfaces";
+import { AllWords, initialWords, OptionalWords } from "../interfaces";
 import { generateWords } from "./generateWords";
 
 const ContentContainer: React.FC = (): ReactElement => {
@@ -15,7 +11,8 @@ const ContentContainer: React.FC = (): ReactElement => {
 	const handleGenerate = async () => {
 		const words = await generateWords();
 		setCurrentWords({
-			...currentWords, ...words
+			...currentWords,
+			...words,
 		});
 	};
 
@@ -31,6 +28,16 @@ const ContentContainer: React.FC = (): ReactElement => {
 		// eslint-disable-next-line
 	}, []);
 
+	const handleClear = () => {
+		setCurrentWords(initialWords);
+		for (let key in currentWords) {
+			const keyInStorage: string | null = localStorage.getItem(key);
+			if (keyInStorage) {
+				localStorage.removeItem(key);
+			}
+		}
+	};
+
 	useEffect(() => {
 		for (let key in currentWords) {
 			if (currentWords[key as keyof OptionalWords]) {
@@ -43,8 +50,12 @@ const ContentContainer: React.FC = (): ReactElement => {
 		<section id="content-container">
 			<div id="story-prompt-container">
 				<div id="prompt-top">
-					<button onClick={handleGenerate}><div id="generateButton"></div></button>
-					<button onClick={() => setCurrentWords(initialWords)}><div id="clearButton"></div></button>
+					<button onClick={handleGenerate}>
+						<div id="generateButton"></div>
+					</button>
+					<button onClick={handleClear}>
+						<div id="clearButton"></div>
+					</button>
 				</div>
 				<StoryPrompts currentWords={currentWords} setCurrentWords={setCurrentWords} />
 			</div>
@@ -52,14 +63,16 @@ const ContentContainer: React.FC = (): ReactElement => {
 				<button onClick={() => setIsStoryHidden(!isStoryHidden)}>
 					{isStoryHidden ? (
 						<img src="openEye.png" alt="an openned eye" />
-						) : (
+					) : (
 						<img src="closeEye.png" alt="a closed eye" />
 					)}
 				</button>
 
 				{isStoryHidden ? (
 					<Story currentWords={currentWords} />
-				) : <div className="story-container"></div>}
+				) : (
+					<div className="story-container"></div>
+				)}
 			</div>
 		</section>
 	);
