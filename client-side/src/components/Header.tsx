dbf9f74ee68b49be203536ea8253e18c1f3e700c
Jaz Kime <JazDKime@gmail.com>
2022-11-30 10:26:53 -0800

    Changed Title to be the hand written words 'Mad Blip' and changed corresponding CSS

--- client-side/src/components/Header.tsx
+++ client-side/src/components/Header.tsx
@@ -1,8 +1,8 @@
 function Header() {
 	return (
 		<header>
-			<h1>Mad Blip</h1>
 			<a href="https://github.com/jzkime/mad-blip" rel="noreferrer" target="_blank">
+				<img src="MadBlip.png" alt="the words 'Mad Blip' written in digital ink" />
 				<img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="github icon" />
 			</a>
 		</header>
